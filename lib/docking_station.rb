786533e46c8027365606a471cae58a67ea7e4634
alexanderwjrussell <awjrussell@live.co.uk>
2016-09-28 11:19:35 +0100

    Chap 13 docking station full error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,14 @@
-
 class DockingStation
+  attr_reader :bike
+
   def release_bike
     fail "error - no bikes available" unless @bike
     @bike
   end
 
   def dock(bike)
+    fail "error - docking station is full" if @bike
     @bike = bike
   end
-
-  attr_reader :bike
+  
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,8 +18,17 @@ require 'spec_helper'
       expect(subject.dock(bike)).to eq bike
     end
 
-    it 'should raise an error if no bikes available' do
-      expect{subject.release_bike}.to raise_error("error - no bikes available")
+    describe '#docking_station' do
+      it 'should raise an error if no bikes available' do
+        expect{subject.release_bike}.to raise_error("error - no bikes available")
+      end
+    end
+
+    describe '#dock' do
+      it "should not accept more bikes than its capacity" do
+        subject.dock(Bike.new)
+        expect{subject.dock(Bike.new)}.to raise_error("error - docking station is full")
+      end
     end
 
 end
