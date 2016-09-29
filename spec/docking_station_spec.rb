dceb7a999a3aeb34bcd234a79e52a605f13c7e16
Darrenf <darrenf54321@gmail.com>
2016-09-29 17:06:47 +0100

    :bike doubles created with all tests passing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
+
   describe DockingStation do
 
    it { is_expected.to respond_to :release_bike}
 
    it "should get a working bike" do
-     bike = Bike.new
+     bike = double(:bike)
+     allow(bike).to receive(:working).and_return(true)
      expect(bike.working).to eq true
    end
 
@@ -14,7 +16,8 @@ require 'spec_helper'
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     it 'returns docked bikes' do
-      bike = Bike.new
+      bike = double(:bike)
+      allow(bike).to receive(:working).and_return(true)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
@@ -27,26 +30,35 @@ require 'spec_helper'
 
     describe '#dock' do
       it "should not accept more bikes than its capacity" do
-        DockingStation::DEFAULT_CAPACITY.times { subject.dock Bike.new }
-        expect{subject.dock(Bike.new)}.to raise_error("error - docking station has exceeded capacity")
+        bike = double(:bike)
+        allow(bike).to receive(:working).and_return(true)
+        DockingStation::DEFAULT_CAPACITY.times { subject.dock bike }
+        expect{subject.dock(bike)}.to raise_error("error - docking station has exceeded capacity")
       end
       it "should recognise a broken bike" do
-        broken_bike= Bike.new
-        broken_bike.report_broken
-        expect(subject.dock(broken_bike)).to eq("Thanks for returning a faulty bike.")
+        bike = double(:bike)
+        allow(bike).to receive(:working).and_return(false)
+          allow(bike).to receive(:report_broken).and_return(false)
+        #broken_bike = bike
+        bike.report_broken
+        expect(subject.dock(bike)).to eq("Thanks for returning a faulty bike.")
       end
     end
 
     describe '#initialize' do
       it "should not accept more bikes than newly specified capacity" do
-        subject.capacity.times { subject.dock Bike.new }
-        expect{subject.dock(Bike.new)}.to raise_error("error - docking station has exceeded capacity")
+        bike = double(:bike)
+        allow(bike).to receive(:working).and_return(true)
+        subject.capacity.times { subject.dock bike }
+        expect{subject.dock(bike)}.to raise_error("error - docking station has exceeded capacity")
       end
 
       it "should not accept more bikes than 50" do
         docking_station = DockingStation.new(50)
-        50.times {docking_station.dock Bike.new}
-        expect{docking_station.dock(Bike.new)}.to raise_error("error - docking station has exceeded capacity")
+        bike = double(:bike)
+        allow(bike).to receive(:working).and_return(true)
+        50.times {docking_station.dock bike}
+        expect{docking_station.dock(bike)}.to raise_error("error - docking station has exceeded capacity")
       end
 
       it 'has a default capacity' do
